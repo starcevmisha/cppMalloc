ce762c119e0c5580f4ceec1d258642b01e77da84
Starcev <starcev_misha@mail.ru>
2018-03-30 22:59:22 +0500

    Имена изменил

--- main.cpp
+++ main.cpp
@@ -10,12 +10,12 @@ struct Descriptor {
     Descriptor *prev;
 };
 
-int has_initialized = 0;
-void *managed_memory_start;
-void *last_valid_address;
+int hasInitialized = 0;
+void *managedMemoryStart;
+void *lastValidAddress;
 
 
-void *alloc(int num_bytes);
+void *alloc(int numBytes);
 
 void free(void *ptr);
 
@@ -25,58 +25,58 @@ void Dump();//Количество блоков
 
 
 void init() {
-    int size = 1000;
-    managed_memory_start = malloc(size);
-    last_valid_address = managed_memory_start + size;
-    has_initialized = 1;
+    int size = 10000;
+    managedMemoryStart = malloc(size);
+    lastValidAddress = managedMemoryStart + size;
+    hasInitialized = 1;
 
-    auto *firstDescriptor = (struct Descriptor *) managed_memory_start;
+    auto *firstDescriptor = (struct Descriptor *) managedMemoryStart;
     firstDescriptor->isFree = true;
     firstDescriptor->size = size - sizeof(struct Descriptor);
     firstDescriptor->prev = NULL;
 
 }
 
-void *alloc(int num_bytes) {
+void *alloc(int numBytes) {
 
-    if (!has_initialized)
+    if (!hasInitialized)
         init();
 
     struct Descriptor *currentDescriptor;
 
-    void *memory_location;
-    memory_location = 0;
+    void *memoryLocation;
+    memoryLocation = 0;
 
-    void *current_location;
-    current_location = managed_memory_start;
+    void *currentLocation;
+    currentLocation = managedMemoryStart;
 
-    while (current_location < last_valid_address) {
-        currentDescriptor = (struct Descriptor *) current_location;
-        if (currentDescriptor->isFree && currentDescriptor->size >= num_bytes) {
+    while (currentLocation < lastValidAddress) {
+        currentDescriptor = (struct Descriptor *) currentLocation;
+        if (currentDescriptor->isFree && currentDescriptor->size >= numBytes) {
             int oldBlockSize = currentDescriptor->size;
             //Нашли блок
             currentDescriptor->isFree = false;
-            currentDescriptor->size = num_bytes;
-            memory_location = current_location + sizeof(Descriptor);
+            currentDescriptor->size = numBytes;
+            memoryLocation = currentLocation + sizeof(Descriptor);
 
-            int available = oldBlockSize - sizeof(Descriptor) - num_bytes;
+            int available = oldBlockSize - sizeof(Descriptor) - numBytes;
 
             if (available > 0) {
-                Descriptor *newDescriptor = (struct Descriptor *) (memory_location + currentDescriptor->size);
+                Descriptor *newDescriptor = (struct Descriptor *) (memoryLocation + currentDescriptor->size);
                 newDescriptor->isFree = true;
                 newDescriptor->size = available;
                 newDescriptor->prev = currentDescriptor;
             }
             break;
         }
-        current_location += currentDescriptor->size + sizeof(Descriptor);
+        currentLocation += currentDescriptor->size + sizeof(Descriptor);
     }
 
-    if (!memory_location)
+    if (!memoryLocation)
         throw std::bad_alloc();
 
-//    memory_location += sizeof(struct Descriptor);
-    return memory_location;
+//    memoryLocation += sizeof(struct Descriptor);
+    return memoryLocation;
 
 }
 
@@ -87,13 +87,12 @@ void free(void *ptr) {
 
     descriptor->isFree = true;
 
+    //Предыдущий дескриптор
     struct Descriptor *next = (Descriptor *) ((char *) descriptor + descriptor->size + sizeof(Descriptor));
-    if (next < last_valid_address && next->isFree) {
+    if (next < lastValidAddress && next->isFree) {
         descriptor->size += next->size + sizeof(Descriptor);
-
-
     }
-
+    //Следующий
     if (descriptor->prev != NULL && descriptor->prev->isFree) {
         int size = descriptor->size + sizeof(Descriptor);
         descriptor = descriptor->prev;
@@ -101,6 +100,7 @@ void free(void *ptr) {
 //        descriptor->prev->size += descriptor->size + sizeof(Descriptor);
     }
 
+    //Меняем ссылку на предыдущий и следующего за следующим
     struct Descriptor *nextByNext = (Descriptor *) ((char *) descriptor + descriptor->size + sizeof(Descriptor));
     nextByNext->prev = descriptor;
     return;
@@ -109,10 +109,10 @@ void free(void *ptr) {
 void Dump() {
     std::cout << endl;
     void *current_location;
-    current_location = managed_memory_start;
+    current_location = managedMemoryStart;
     struct Descriptor *currentDescriptor;
 
-    while (current_location < last_valid_address) {
+    while (current_location < lastValidAddress) {
         currentDescriptor = (struct Descriptor *) current_location;
         std::cout << currentDescriptor->size << ' ' << currentDescriptor->isFree << endl;
         current_location += currentDescriptor->size + sizeof(Descriptor);
